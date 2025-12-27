87303b7f388eee4493a1e70e3a26e6edc266cc49
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-27 18:39:44 +0500

    done

---
+++ JS/Takrorlash/main.js
@@ -0,0 +1,88 @@
+// function salom() {
+//     console.log("Salom dunyo");
+// }
+// salom()
+
+
+// function sum(a, b) {
+//     console.log(a + b);
+// }
+// sum(5, 6)
+// sum(8, 7)
+
+
+// function kvadrat(x) {
+//     return x * x;
+// }
+// let result = kvadrat(5)
+// console.log(result);
+
+
+// function yoshHisobla(tugilganYil) {
+//     return 2025 - tugilganYil;
+// }
+
+// console.log(yoshHisobla(2010));
+
+
+// let yosh = prompt("Tug'ilgan yilingni kirit")
+// function yoshHisobla(tugilganYil) {
+//     console.log(2025 - tugilganYil);
+// }
+// yoshHisobla(yosh)
+
+// let hozirgiYil = prompt("hozirgi yilni kirit")
+// let tugilganYil = prompt("tugilgan yilingni kirit")
+// function tugilganYilHisoblagich(a, b) {
+//     return a - b
+// }
+// let result = tugilganYilHisoblagich(hozirgiYil, tugilganYil)
+// console.log("Sen " + result + ' yoshdasan');
+
+
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+// console.log("Salom dunyo");
+
+// for (let abduvali = 0; abduvali < 10; abduvali++) {
+//     console.log("salom dunyo");
+// }
+
+
+// let arr = [10, 20, 30];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+
+// let arr = [10, 20, 30, 55, 86];
+
+// arr.forEach((item, index) => {
+//     console.log(item, index);
+// });
+
+
+// let arr = [1, 2, 3, 4, 5];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] === 5) break;
+//     console.log(arr[i]);
+// }
+
