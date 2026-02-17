5ac8bf7d9d7932f1ad3d1d96db10d467ce0a4577
ibrohim3 <ibrohim.2128382@gmail.com>
2026-02-17 16:17:17 +0500

    done

---
+++ 14 - 16 JS SPSH/import export/helpers.js
@@ -0,0 +1,33 @@
+// function salom() {
+//     alert("helpers ishladi")
+// }
+
+
+// export function salom() {
+//     alert("helpers ishladi")
+// }
+
+export function add(a, b) {
+    return a + b
+}
+
+export function max(c, d) {
+    if (c > d) return c
+    else if (d > c) return d
+    else return "2 ta son xam teng"
+}
+
+// export function max(c, d) {
+//     if (c >= d) return c
+//     return d
+// }
+
+// export function max2(c, d) {
+//     if (c > d) {
+//         alert(`katta son ${c}`)
+//     } else if (d > c) {
+//         alert(`katta son ${d}`)
+//     } else {
+//         alert("2 ta son xam teng")
+//     }
+// }
---
+++ 14 - 16 JS SPSH/import export/main.js
@@ -0,0 +1,21 @@
+// alert("main ishladi")
+// salom()
+
+
+// import { salom } from './helpers.js'
+// alert("hello main ishladi")
+// salom()
+import { add } from "./helpers.js";
+import { max } from "./helpers.js";
+
+// let a = Number(prompt("a son kirit"))
+// let b = Number(prompt("b son kirit"))
+
+// let result = add(a, b)
+// alert(`javobi ${result}`)
+
+let c = Number(prompt("c son kirit"))
+let d = Number(prompt("d son kirit"))
+
+let result = max(c, d)
+alert(`Eng katta son: ${result}`)
