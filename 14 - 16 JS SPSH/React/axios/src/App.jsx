70eab8abe7db100fcae3c5e6f1a022fe5458f785
ibrohim3 <ibrohim.2128382@gmail.com>
2026-04-21 15:48:29 +0500

    done

---
+++ 14 - 16 JS SPSH/React/axios/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import api from "./utils/axios";
+function App() {
+  const [allPosts, setAllPosts] = useState([]);
+  const fetchUser = async () => {
+    try {
+      const res = await api.get("/posts");
+      setAllPosts(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchUser();
+  }, []);
+  return (
+    <div>
+      {allPosts.map((item, index) => {
+        return <div key={index}>{item.title}</div>;
+      })}
+    </div>
+  );
+}
+
+export default App;
